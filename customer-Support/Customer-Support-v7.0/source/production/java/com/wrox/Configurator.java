8a7051bc93a8ada843edd9056a78691d9c3a1406
dw2750498 <dw2750498@163.com>
2019-03-08 12:14:50 +0800

    第一次提交

---
+++ customer-Support/Customer-Support-v7.0/source/production/java/com/wrox/Configurator.java
@@ -0,0 +1,29 @@
+package com.wrox;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class Configurator implements ServletContextListener
+{
+    //声明了authenticationFilter并将它映射到/tickets和/sessions
+    @Override
+    public void contextInitialized(ServletContextEvent event)
+    {
+        ServletContext context = event.getServletContext();
+
+        FilterRegistration.Dynamic registration = context.addFilter(
+                "authenticationFilter", new AuthenticationFilter()
+        );
+        registration.setAsyncSupported(true);
+        registration.addMappingForUrlPatterns(
+                null, false, "/tickets", "/sessions"
+        );
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) { }
+}
